c022945a456ac69986fbdd3fcf0e38f9244b7619
Ayo <ayocre8tiv@gmail.com>
2024-12-09 12:02:27 +0100

    algorithms

---
+++ index.js
@@ -0,0 +1,27 @@
+// Adding Three Numbers 
+
+var num1 = 17
+var num2 = 25
+var num3 = 12
+
+var sum = num1 + num2 + num3
+
+console.log(sum)
+
+
+
+// Find Percentage Profit
+
+var cp = 12500
+var sp = 72750
+
+
+// Selection Processing
+
+var age = 25
+if(age>17){
+    console.log("You can vote")
+}else{
+    console.log("You can't vote")
+}
+
